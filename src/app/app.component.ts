878552d7a07a1a038ff3d18922e2edc337f899d9
MarianoJuly <marcianocruzeiri@gmail.com>
2023-06-22 21:24:29 -0300

    mensagem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MensageService } from './services/mensage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Passagens_Aereas';
+
+  constructor(private mensagem: MensageService){}
+
+  ngOnInit(): void {};
+
+  teste(){
+    this.mensagem.add("Teste");
+  }
 }
